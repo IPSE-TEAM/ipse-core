0e1c4fd248c8f74503e1fab208de3fc4d75a477a
Lawliet-Chan <lawliet@lawlietdeiMac-Pro.local>
2020-06-05 17:32:58 +0800

    add balance module

--- bin/node/runtime/src/ipse.rs
+++ bin/node/runtime/src/ipse.rs
@@ -4,41 +4,52 @@ extern crate frame_system as system;
 extern crate pallet_timestamp as timestamp;
 
 use codec::{Decode, Encode};
+use frame_support::traits::{Currency, LockableCurrency, WithdrawReasons};
 use frame_support::{
-    decl_event, decl_module, decl_storage,
-    decl_error, dispatch::DispatchResult, debug,
-    ensure, weights::{SimpleDispatchInfo},
+    debug, decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult, ensure,
+    weights::SimpleDispatchInfo,
 };
-use system::{ensure_signed};
 use sp_runtime::traits::SaturatedConversion;
-use sp_std::vec::Vec;
 use sp_std::convert::TryInto;
+use sp_std::vec::Vec;
+use system::ensure_signed;
 
-pub const MB: u64 = 1024 * 1024;
+pub const KB: u64 =  1024;
+/// Miner locks some funds per KB for staking.
+pub const STAKING_PER_KB: BalanceOf<dyn Trait> = 1000;
+/// Lock some funds of user.
+pub const USER_LOCK: [u8; 8] = *b"user    ";
+/// Lock some funds of miner.
+pub const MINER_LOCK: [u8; 8] = *b"miner   ";
+
+pub type BalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
 
 pub trait Trait: system::Trait + timestamp::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+    type Currency: LockableCurrency<Self::AccountId, Moment = Self::BlockNumber>;
 }
 
 #[derive(Encode, Decode, Clone, Debug, Default, PartialEq, Eq)]
-pub struct Miner {
+pub struct Miner<Balance> {
     pub nickname: Vec<u8>,
     // where miner server locates
     pub region: Vec<u8>,
     // the miner's url
     pub url: Vec<u8>,
     pub capacity: u64,
-    // price per MB
-    pub unit_price: u64,
+    // price per KB
+    pub unit_price: Balance,
 }
 
 #[derive(Encode, Decode, Clone, Debug, Default, PartialEq, Eq)]
-pub struct Order<AccountId> {
+pub struct Order<AccountId, Balance> {
+    // the key of this data
     pub key: Vec<u8>,
     // the merkle root of data
     pub merkle_root: Vec<u8>,
     pub user: AccountId,
-    pub orders: Vec<MinerOrder<AccountId>>,
+    pub orders: Vec<MinerOrder<AccountId, Balance>>,
     pub status: OrderStatus,
     // last update-status timestamp
     pub update_ts: u64,
@@ -47,9 +58,9 @@ pub struct Order<AccountId> {
 }
 
 #[derive(Encode, Decode, Clone, Debug, Default, PartialEq, Eq)]
-pub struct MinerOrder<AccountId> {
+pub struct MinerOrder<AccountId, Balance> {
     pub miner: AccountId,
-    pub total_price: u64,
+    pub total_price: Balance,
     // last verify result
     pub verify_result: bool,
     // last verify timestamp
@@ -69,22 +80,31 @@ pub enum OrderStatus {
 
 decl_storage! {
     trait Store for Module<T: Trait> as Ipse {
-        pub Miners get(miner): map hasher(twox_64_concat) T::AccountId => Miner;
+        pub Miners get(miner): map hasher(twox_64_concat) T::AccountId => Miner<BalanceOf<T>>;
         // order id is the index of vec.
-        pub Orders get(order): Vec<Order<T::AccountId>>;
+        pub Orders get(order): Vec<Order<T::AccountId, BalanceOf<T>>>;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-    	type Error = Error<T>;
+        type Error = Error<T>;
 
         fn deposit_event() = default;
 
         #[weight = SimpleDispatchInfo::FixedNormal(10_000)]
-        fn register_miner(origin, nickname: Vec<u8>, region: Vec<u8>, url: Vec<u8>, capacity: u64, unit_price: u64) {
+        fn register_miner(origin, nickname: Vec<u8>, region: Vec<u8>, url: Vec<u8>, capacity: u64, unit_price: BalanceOf<T>) {
             let miner = ensure_signed(origin)?;
+            let total_staking = capacity * STAKING_PER_KB / KB;
+            // lock for staking
 
+            Miners::<T>::insert(&miner, Miner {
+                nickname,
+                region,
+                url,
+                capacity,
+                unit_price,
+            });
         }
 
         #[weight = SimpleDispatchInfo::FixedNormal(10_000)]
@@ -93,7 +113,7 @@ decl_module! {
             let mut miner_orders = Vec::new();
             for m in miners {
                 let miner = Self::miner(&m).ok_or(Error::<T>::MinerNotFound)?;
-                let total_price = miner.unit_price * data_length / MB;
+                let total_price = miner.unit_price * data_length / KB;
                 let miner_order = MinerOrder {
                     miner,
                     total_price,
@@ -122,8 +142,13 @@ decl_module! {
                 let mut order = os.get_mut(order_id).ok_or(Error::<T>::OrderNotFound)?;
                 let mut miner_order = Self::find_miner_order(miner, order.orders).ok_or(Error::<T>::MinerOrderNotFound)?;
                 miner_order.confirm_ts = Self::get_now_ts();
-                // lock user's balance
-
+                // lock some user's balance
+                T::Currency::set_lock(
+                    USER_LOCK,
+                    &order.user,
+                    miner_order.total_price,
+                    WithdrawReasons::all()
+                );
                 Ok(())
             })?;
         }
@@ -137,8 +162,8 @@ decl_module! {
                 ensure!(&order.status == &OrderStatus::Expired, Error::<T>::OrderExpired);
                 order.status = OrderStatus::Deleted;
                 order.update_ts = Self::get_now_ts();
-                // unlock user's balance
-
+                // unlock some user's balance
+                T::Currency::remove_lock(USER_LOCK, &order.user);
                 OK(())
             })?;
         }
@@ -164,13 +189,16 @@ impl<T: Trait> Module<T> {
         <T::Moment as TryInto<u64>>::try_into(now).ok().unwrap()
     }
 
-    fn find_miner_order(miner: T::AccountId, os: Vec<MinerOrder<T::AccountId>>) -> Option<MinerOrder<T::AccountId>>{
+    fn find_miner_order(
+        miner: T::AccountId,
+        os: Vec<MinerOrder<T::AccountId, BalanceOf<T>>>,
+    ) -> Option<MinerOrder<T::AccountId, BalanceOf<T>>> {
         for o in os {
             if o.miner == miner {
-                return Some(o)
+                return Some(o);
             }
         }
-        return None
+        return None;
     }
 }
 
