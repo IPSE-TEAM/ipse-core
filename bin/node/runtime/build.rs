b5e80f720768550cc8a2884341282d766a5c1d75
transxask <orwx71@163.com>
2021-05-11 16:05:14 +0800

    update

--- bin/node/runtime/build.rs
+++ bin/node/runtime/build.rs
@@ -18,10 +18,10 @@
 use wasm_builder_runner::WasmBuilder;
 
 fn main() {
-	WasmBuilder::new()
-		.with_current_project()
-		.with_wasm_builder_from_crates_or_path("2.0.0", "../../../utils/wasm-builder")
-		.export_heap_base()
-		.import_memory()
-		.build()
+//	WasmBuilder::new()
+//		.with_current_project()
+//		.with_wasm_builder_from_crates_or_path("2.0.0", "../../../utils/wasm-builder")
+//		.export_heap_base()
+//		.import_memory()
+//		.build()
 }
--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -95,17 +95,17 @@ pub mod ipse;
 pub mod exchange;
 pub mod ocw_common;
 
-// Make the WASM binary available.
-#[cfg(feature = "std")]
-include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+//// Make the WASM binary available.
+//#[cfg(feature = "std")]
+//include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-#[cfg(feature = "std")]
-/// Wasm binary unwrapped. If built with `BUILD_DUMMY_WASM_BINARY`, the function panics.
-pub fn wasm_binary_unwrap() -> &'static [u8] {
-	WASM_BINARY.expect("Development wasm binary is not available. This means the client is \
-						built with `BUILD_DUMMY_WASM_BINARY` flag and it is only usable for \
-						production chains. Please rebuild with the flag disabled.")
-}
+//#[cfg(feature = "std")]
+///// Wasm binary unwrapped. If built with `BUILD_DUMMY_WASM_BINARY`, the function panics.
+//pub fn wasm_binary_unwrap() -> &'static [u8] {
+//	WASM_BINARY.expect("Development wasm binary is not available. This means the client is \
+//						built with `BUILD_DUMMY_WASM_BINARY` flag and it is only usable for \
+//						production chains. Please rebuild with the flag disabled.")
+//}
 
 /// Runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
